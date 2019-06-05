6c427392aa0814f00c3c9759dacbc41e6a4b5eb9
zainhaq34 <zainhaq@outlook.com>
2019-06-06 04:56:30 +0500

    Skill Screen Layout

--- app/src/main/java/com/example/swoosh/SkillActivity.kt
+++ app/src/main/java/com/example/swoosh/SkillActivity.kt
@@ -3,10 +3,10 @@ package com.example.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SkillActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_skill)
     }
 }
---
+++ app/src/main/java/com/example/swoosh/WelcomeActivity.kt
@@ -0,0 +1,18 @@
+package com.example.swoosh
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        league_next_btn.setOnClickListener{
+            startActivity(Intent(this, LeagueActivity::class.java))
+        }
+    }
+}
