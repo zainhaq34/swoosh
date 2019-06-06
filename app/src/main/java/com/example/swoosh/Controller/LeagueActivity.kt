7f49c113974e1678fbaf3c6f83451b59b465711e
zainhaq34 <zainhaq@outlook.com>
2019-06-06 06:26:16 +0500

    Some Bug Fixed

--- app/src/main/java/com/example/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/Controller/LeagueActivity.kt
@@ -3,6 +3,7 @@ package com.example.swoosh.Controller
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.example.swoosh.Model.Player
@@ -14,9 +15,29 @@ class LeagueActivity : AppCompatActivity() {
 
     var player = Player("", "")
 
+    /** Save Instance Player class Pramas when rotating screen or app pause state
+     * 1. this method must be written above onCreate method
+     * otherwise does not work
+     */
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        // put player bundle into outState
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+    /** Restore Instance Player class Pramas when rotating screen or app pause state
+     * 1. this method must be written below onCreate method
+     * otherwise does not work
+     */
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
     fun onMensClicked(view: View) {
